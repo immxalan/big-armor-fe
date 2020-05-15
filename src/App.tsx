5926dc09868329ae90ef687a3787970a9aa07c95
Marco Whyte <mwhyte@umass.edu>
2020-05-15 16:08:39 -0500

    try it page navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from "./pages/Home";
 import API from "./pages/API";
 import Model from "./pages/Model";
 import About from "./pages/About";
+import TryIt from "./pages/TryIt";
 
 function App() {
   return (
@@ -14,6 +15,9 @@ function App() {
         <Switch>
           <Route exact path="/">
             <Home />
+          </Route>
+          <Route exact path="/try-it">
+            <TryIt />
           </Route>
           <Route path="/api-docs">
             <API />
---
+++ src/pages/TryIt.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import NavBar from "../components/NavBar";
+
+interface Props {}
+
+const TryIt = () => {
+  return (
+    <div>
+      <NavBar backgroundTransparent={false} />
+    </div>
+  );
+};
+
+export default TryIt;
